a95a0a2d2ac6536d3531cc6f059f77d96f03d3a0
Samuel Kofi Afrifa <afrifasamuel728@gmail.com>
2026-05-31 14:33:12 +0000

    Complete Assignment 1

--- initials.py
+++ initials.py
@@ -1,7 +1,35 @@
+# File: initials.py
+# Description: Print out my initials in large stylized letters.
+# Assignment Number: 1
+#
+# Name: Afrifa Samuel Kofi
+# STUDENT ID: 2425401293
+# Email: 2425401293@live.gctu.edu.gh
+# Grader: Augustus
+#
+# On my honor, Samuel Kofi Afrifa, this programming assignment is my own work
+# and I have not provided this code to any other student.
+
 
 def main():
-  pass #code goes here
+    # Print my initials in large stylized letters.
 
+    print()
+    print("...ASK")
+    print()
+
+    print("...AAAAAAAAAAAA........SSSSSSSSSSSS........KKKKKK..kKKK.....")
+    print("...AAAA....AAAA........SSSSSSSSSSSS........KKKKK..kkKK......")
+    print("...AAAA....AAAA........SSSS................KKKKK.KKKK.......")
+    print("...AAAA....AAAA........SSSS................KKKKKKKKK........")
+    print("...AAAAAAAAAAAA........SSSSSSSSSSSS........KKKKKKKK.........")
+    print("...AAAAAAAAAAAA........SSSSSSSSSSSS........KKKKKKKK.........")
+    print("...AAAA....AAAA................SSSS........KKKKKKKKK........")
+    print("...AAAA....AAAA................SSSS........KKKKK.KKKK.......")
+    print("...AAAA....AAAA........SSSSSSSSSSSS........KKKKK..KkKK......")
+    print("...AAAA....AAAA........SSSSSSSSSSSS........KKKKK...kKKK.....")
+
+    print()
 
 
 main()
